f3aefcf899810a69e301c1948555e644c373d871
Ömer Ayılmazdır <omeraylmazdr@gmail.com>
2023-06-24 22:40:28 +0300

    24.06.2023

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import UserList from './components/UserList';
+import UserDetail from './components/UserDetail';
+import { useState } from 'react';
 
 function App() {
+  const [activeUserId, setActiveUserId] = useState(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <UserList setActiveUserId={setActiveUserId} />
+      </div>
+      <div>
+        {
+          activeUserId && (
+            <UserDetail activeUserId={activeUserId} />
+          )
+        }
+      </div>
     </div>
   );
 }
